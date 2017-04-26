a42ce70432d73fe8cead72815be4d0b5894acb5a
Krating <c.kullawat@gmail.com>
2017-04-26 11:39:27 +0700

    commit

--- app.js
+++ app.js
@@ -36,11 +36,8 @@ app.set('view engine','ejs');
 
 app.use(express.static(__dirname + ''));
 
-var port = process.env.PORT || 8081;
-
-server.listen((port), function(){
-	console.log('Server started on port '+ port);
-});
+var port = process.env.PORT || 8000;
+server.listen(port);
 
 // BodyParser Middleware
 app.use(bodyParser.json());
